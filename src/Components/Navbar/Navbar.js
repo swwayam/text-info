f6126dd139660e8ad67bf3e6b71b420fe3fbaacf
swwayam <codingswayam24@gmail.com>
2022-12-10 13:21:00 +0530

    deployment with dark mode

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import './Navbar.css'
 
-const Navbar = () => {
+const Navbar = (props) => {
     return(
-        <nav className='nav-light'>
+        <nav className={`nav-${props.theme}`}>
             <ul >
                 <li className='logo'><a className='a-light' href="/">textInfo</a></li>
                 <li ><a className='a-light' href="/" >About us</a></li>
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import "./Home.css";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 const Home = () => {
   const [content, setContent] = useState("");
+  const [theme, setTheme] = useState("dark");
 
   const capitalize = () => {
     setContent((oldContent) => oldContent.toUpperCase());
@@ -22,7 +24,7 @@ const Home = () => {
 
   const capFirstChar = () => {
     setContent((oldContent) => {
-      const str = oldContent.split(" ");
+      const str = oldContent.trim().split(" ");
       const capFirst = str.map(
         (e) => e[0].toUpperCase() + e.slice(1).toLowerCase()
       );
@@ -34,10 +36,20 @@ const Home = () => {
     navigator.clipboard.writeText(content);
   };
 
+  const changeTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+    console.log(theme);
+  };
+
   return (
-    <div>
-      <Navbar />
-      <div className="home">
+    <div className={`div home-${theme}`}>
+      {theme === "light" ? (
+        <BsMoon className="icon moon" onClick={changeTheme} />
+      ) : (
+        <BsSun className="icon sun" onClick={changeTheme} />
+      )}
+      <Navbar theme={theme} />
+      <div className="home ">
         <textarea
           onChange={(e) => {
             setContent(e.target.value);
@@ -47,13 +59,25 @@ const Home = () => {
           rows="30"
           placeholder="Enter Your Text"
         ></textarea>
-        <div className="btn-grp">
-          <button onClick={capitalize}>Captilize All Text</button>
-          <button onClick={lowerCase}>Lower Case All Text</button>
-          <button onClick={capFirstChar}>Captilize First Character</button>
-          <button onClick={copyToClip}>Copy to Clip Board</button>
-          <button>Charecter Count: {content.length}</button>
-          <button>WordCount: {content.length > 1 ? content.split(" ").length : "0"} </button>
+        <div className="btn-grp ">
+          <button className={`btn-${theme}`} onClick={capitalize}>
+            Captilize All Text
+          </button>
+          <button className={`btn-${theme}`} onClick={lowerCase}>
+            Lower Case All Text
+          </button>
+          <button className={`btn-${theme}`} onClick={capFirstChar}>
+            Captilize First Character
+          </button>
+          <button className={`btn-${theme}`} onClick={copyToClip}>
+            Copy to Clip Board
+          </button>
+          <button className={`btn-${theme}`}>
+            Charecter Count: {content.length}
+          </button>
+          <button className={`btn-${theme}`}>
+            WordCount: {content.length > 1 ? content.split(" ").length : "0"}{" "}
+          </button>
         </div>
       </div>
     </div>
